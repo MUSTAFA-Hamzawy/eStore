64fdfedd0388c7ce7cd144672dc65b6f08ca1994
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-14 18:24:18 +0200

    Modified some views

--- app/views/dashboard/footer.php
+++ app/views/dashboard/footer.php
@@ -1,8 +1,5 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>By : <a href="#">Mustafa Hamzawy</a>.</strong> All rights reserved.
 </footer>
 
 <!-- Control Sidebar -->
@@ -14,12 +11,12 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?= BACK_ASSETS ?>/plugins/jquery/jquery.min.js"></script>
+<script src="<?= BACK_ASSETS ?>plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="<?= BACK_ASSETS ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= BACK_ASSETS ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= BACK_ASSETS ?>/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?= BACK_ASSETS ?>/dist/js/demo.js"></script>
+<script src="<?= BACK_ASSETS ?>dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes-->
+<script src="<?= BACK_ASSETS ?>dist/js/demo.js"></script>
 </body>
 </html>
