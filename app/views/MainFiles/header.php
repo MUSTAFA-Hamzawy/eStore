102fae1b77d481faa089cf60d4b52fb978f6e515
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-15 21:43:08 +0200

    Added the page title as a variable for each page

--- app/views/MainFiles/header.php
+++ app/views/MainFiles/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blank Page</title>
+    <title><?= $this->pageTitle; ?></title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="<?= BACK_ASSETS ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= BACK_ASSETS ?>dist/css/adminlte.min.css">
+<!--    todo-me add title and faveicon-->
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
