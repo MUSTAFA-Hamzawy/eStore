5c16bdb144ed62bd61b1a19af1261624863c31b5
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-15 22:39:45 +0200

    nothing

--- app/controllers/privileges.php
+++ app/controllers/privileges.php
@@ -20,8 +20,8 @@ class privileges extends controller
 
   public function main()
   {
-    if (empty($_SESSION))
-      helpers::reDirect("../admin");
+    if (empty($_SESSION))   // todo-me : another way to check that the person is logged in
+      helpers::reDirect("../admin");  // todo-me: think of good way to get the path not written manually
 
     $this->pageTitle = 'Privileges';
     $this->view();
@@ -30,10 +30,11 @@ class privileges extends controller
   public function add(){
 
     if (isset($_POST['submit'])) {
-
+      // todo-me: make the field is required by js to avoid empty
       $this->model->Name = $this->sanitizeString($_POST['privilege']);
       $this->model->url_title = $this->sanitizeString($_POST['link']);
-      if ($this->model->add())
+
+      if ($this->model->add())    // todo: show the massages to user for 5 seconds
         $this->addMessageToUser('success', "Privilege is added successfully.");
       else
         $this->addMessageToUser('errors', "Failed to add this privilege.");
@@ -42,30 +43,37 @@ class privileges extends controller
     $this->pageTitle = "Add Privilege";
     $this->view();
   }
+
   public function edit(){
+
+    $this->pageTitle = "Edit Privilege";
+    $this->model->id = $this->parameters[0];    // todo-me : sanitize the parameters first
+    $this->data = $this->model->fetchRecord();
+
+    $this->view();
 
     if (isset($_POST['submit'])) {
 
       $this->model->Name = $this->sanitizeString($_POST['privilege']);
       $this->model->url_title = $this->sanitizeString($_POST['link']);
-      if ($this->model->add())
-        $this->addMessageToUser('success', "Privilege is added successfully.");
+      if ($this->model->edit())
+        $this->addMessageToUser('success', "Privilege is Updated successfully.");
       else
-        $this->addMessageToUser('errors', "Failed to add this privilege.");
+        $this->addMessageToUser('errors', "Failed to update this privilege.");
+
+      helpers::reDirect("../test");     // todo-me : redirect after editing to the home page and show the msgs
     }
 
-    $this->pageTitle = "Add Privilege";
-    $this->view();
   }
 
   public function delete(){
-    $this->model->id = $this->parameters[0];
+    $this->model->id = $this->parameters[0]; // todo-me : sanitize the parameters first
 
     if($this->model->delete())
       $this->addMessageToUser('success', "Privilege is Removed successfully.");
     else
       $this->addMessageToUser('errors', "Failed to remove this privilege.");
 
-    helpers::reDirect("../");
+    helpers::reDirect("../");// todo-me : redirect after deleting to the home page and show the msgs
   }
 }
