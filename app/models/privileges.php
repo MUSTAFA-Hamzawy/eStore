0dc748b9ea2903f871e5db0bb05ae063fcebd962
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-15 22:39:38 +0200

    Added some db functions

--- app/models/privileges.php
+++ app/models/privileges.php
@@ -23,6 +23,16 @@ class privileges extends model
     return $this->db->rows("SELECT * FROM " . self::tableName);
   }
 
+  /**
+   * This function fetches the data of the a record that has primary key value equals to the data member id
+   * @return object
+   */
+  public function fetchRecord(){
+    $name = self::tableName;
+    $query = "SELECT * FROM {$name} where {$this->primaryKey} = ?";
+    return $this->db->row($query, [$this->id]);
+  }
+
   public function add(){
     $data = array_combine($this->tableSchema, [$this->Name, $this->url_title]);
     $check = $this->db->insert(self::tableName, $data);
@@ -30,7 +40,9 @@ class privileges extends model
   }
 
   public function edit(){
-
+    $data = array_combine($this->tableSchema, [$this->Name, $this->url_title]);
+    $condition = [$this->primaryKey => $this->id];
+    return $this->db->update(self::tableName, $data, $condition);
   }
 
   public function delete(){
