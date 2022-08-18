54a62c86885f96d004476240ec8c54c421e48ec5
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-18 21:39:20 +0200

    Separated the config file

--- public/index.php
+++ public/index.php
@@ -1,30 +1,6 @@
 <?php
 use MVC\core\UrlHandler;
 require_once "../vendor/autoload.php";
-
-// config
-define("ROOT", dirname(__DIR__));
-define("APP", ROOT . DIRECTORY_SEPARATOR . "app");
-define("CONTROLLERS", APP . DIRECTORY_SEPARATOR . "controllers");
-define("MODELS", APP . DIRECTORY_SEPARATOR . "models");
-define("VIEWS", APP . DIRECTORY_SEPARATOR . "views");
-define("DOMAIN", "localhost");
-define("ROOT_LINK", "http://localhost/pages/eStore/public/");
-
-// front files paths
-define("PUBLIC_DIR", ROOT . DIRECTORY_SEPARATOR . "public");
-define("FRONT_ASSETS", "http://localhost/pages/eStore/public/front/");
-define("BACK_ASSETS", "http://localhost/pages/eStore/public/back/");
-
-// some needed files
-define("HEADER", VIEWS . DIRECTORY_SEPARATOR . "MainFiles" . DIRECTORY_SEPARATOR . "header.php");
-define("SIDEBAR", VIEWS . DIRECTORY_SEPARATOR . "MainFiles" . DIRECTORY_SEPARATOR . "sidebar.php");
-
-// database info
-define("DATABASE_NAME", 'store');
-define("PASSWORD", '');
-define("USER_NAME", 'root');
-define("HOST_NAME", 'localhost');
-define("PORT", 3306);
+require_once "config.php";
 
 $app = new UrlHandler();
