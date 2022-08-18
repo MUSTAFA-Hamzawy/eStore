d78851c349b9caccaba101568268ed52f981a096
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-18 21:59:59 +0200

    Organized the code

--- app/controllers/privileges.php
+++ app/controllers/privileges.php
@@ -14,13 +14,13 @@ class privileges extends controller
 
     $this->viewFolderName = "privileges";
     $this->model = new privilegeModel();
-    $this->data = $this->model->fetchRecords();
+    $this->data = $this->model->fetchModelRecords();
   }
 
   public function main()    // default method
   {
-    if (empty($_SESSION))   // todo-me : another way to check that the person is logged in
-      helpers::reDirect("admin");  // todo-me: think of good way to get the path not written manually
+    if (empty($_SESSION))
+      helpers::reDirect("admin");
 
     $this->pageTitle = 'Privileges';
     $this->method = "main";
@@ -29,18 +29,18 @@ class privileges extends controller
 
   public function add(){
 
+    $this->pageTitle = "Add Privilege";
+
     if (isset($_POST['submit'])) {
-      // todo-me: make the field is required by js to avoid empty
       $this->model->Name = $this->sanitizeString($_POST['privilege']);
       $this->model->url_title = $this->sanitizeString($_POST['link']);
 
-      if ($this->model->add())    // todo: show the massages to user for 5 seconds
-        $this->addMessageToUser('success', "Privilege is added successfully.");
+      if ($this->model->add())
+        $this->addMessageToUser('success', "Privilege has been added successfully.");
       else
         $this->addMessageToUser('errors', "Failed to add this privilege.");
     }
 
-    $this->pageTitle = "Add Privilege";
     $this->view();
   }
 
