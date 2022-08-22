80b591f845af6c4d87675a937c487fa1984a9b3d
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-22 13:16:46 +0200

    Added Messenger class which handles the messages between the UI and Backend

---
+++ app/core/Messenger.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace MVC\core;
+
+
+class Messenger
+{
+  const SUCCESS_MESSAGE = "Success";
+  const ERROR_MESSAGE   = "Error";
+  const WARNING_MESSAGE = "Warning";
+
+  private static $instance;
+  private static $messeges;
+
+  private function __construct() { }
+
+  private function __clone(){
+
+  }
+
+  public static function getInstance(){
+    if (! isset(self::$instance))
+    {
+      self::$instance = new self();
+    }
+
+    return self::$instance;
+  }
+
+  private function messageExist($msg){
+    return isset($this->session->messages);
+}
+
+  public function addMessage($msg, $msgType = self::SUCCESS_MESSAGE){
+    if (!$this->messageExist($msg))
+    {
+      self::$messeges[] = [$msgType, $msg];
+    }
+  }
+
+  public function clearMessages(){
+//    unset(self::$messeges);
+  }
+
+  public function getMesseges(){
+    return self::$messeges;
+  }
+
+}
