0c562e3fbdb0b6faebe709b2d622492701e03062
MUSTAFA-Hamzawy <hamzway2019579@gmail.com>
2022-08-22 19:19:28 +0200

    Added The user entity to the system.

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -2,24 +2,250 @@
 
 
 namespace MVC\controllers;
+use Cassandra\Exception\ValidationException;
 use MVC\core\helpers;
+use MVC\core\Messenger;
 use MVC\core\session;
 use MVC\core\validation;
+use MVC\models\group;
 use MVC\models\model;
 use MVC\models\user as userModel;
 
 
 class user extends controller
 {
-    public function __construct(){
-      parent::__construct();
+    private $groupModel;
+    public function __construct($db){
+      parent::__construct($db);
+
       $this->viewFolderName = 'user';
-      $this->model = new userModel();
+      $this->model = new userModel($db);
+      $this->groupModel = new group($db);
     }
 
     public function main()
     {
-        helpers::reDirect("user/login");
+//      $this->data['myData'] = $this->model->fetchModelRecords();
+      $this->data['mainData'] = $this->model->getUserGroups();
+      $this->pageTitle = 'Users';
+      $this->method = "main";
+      $this->view();
+    }
+    private function validateChosenGroup(){
+//      print_r($_POST);die;
+      $selectedGroupId = filter_var($_POST['selectedGroup'], FILTER_SANITIZE_NUMBER_INT);
+      if (empty($selectedGroupId) || ! is_integer((int)$selectedGroupId))
+        $this->messenger->addMessage("Invalid Chosen Group", Messenger::ERROR_MESSAGE);
+      else
+        $this->model->groupId = $selectedGroupId;
+    }
+
+    private function validateEmail(){
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+    // check not empty
+    if (empty($email))
+    {
+      $this->messenger->addMessage("Email is required", Messenger::ERROR_MESSAGE);
+      return false;
+    }
+
+    // check data type
+    if (! validation::isEmail($email))
+    {
+      $this->messenger->addMessage("Invalid Email", Messenger::ERROR_MESSAGE);
+      return false;
+    }
+
+    $this->model->email = $email;
+    return true;
+  }
+
+    private function validateUsername(){
+      $username = validation::sanitizeString($_POST['username']);
+
+      // check not empty
+      if (empty($username))
+      {
+        $this->messenger->addMessage("username is required", Messenger::ERROR_MESSAGE);
+        return false;
+      }
+
+      // check data type
+      if (! is_string($username))
+      {
+        $this->messenger->addMessage("Invalid Username", Messenger::ERROR_MESSAGE);
+        return false;
+      }
+
+      // check length
+      if (! validation::between(strlen($username),5, 25))
+      {
+        $this->messenger->addMessage("username length must be between 5 and 25", Messenger::ERROR_MESSAGE);
+        return false;
+      }
+
+      $this->model->username = $username;
+      return true;
+    }
+
+    private function checkPasswordPower($password){
+    if (strlen($password) < 8)
+    {
+      $this->messenger->addMessage("Write more powerful password", Messenger::WARNING_MESSAGE);
+      return false;
+    }
+
+    if (! preg_match("/[a-z]/i", $password) || ! preg_match("/[0-9]/", $password))
+    {
+        $this->messenger->addMessage("Password must be mix of characters and numbers", Messenger::WARNING_MESSAGE);
+        return false;
+    }
+
+    if (! preg_match("/[A-Z]/", $password))
+    {
+        $this->messenger->addMessage("Password must have at least one Capital character", Messenger::WARNING_MESSAGE);
+        return false;
+    }
+
+    return true;
+  }
+
+    private function validatePassword(){
+    $password = validation::sanitizeString($_POST['password']);
+
+    // check not empty
+    if (empty($password))
+    {
+      $this->messenger->addMessage("Password is required", Messenger::ERROR_MESSAGE);
+      return false;
+    }
+
+    // check data type
+    if (! is_string($password))
+    {
+      $this->messenger->addMessage("Password must be only alphanumeric", Messenger::ERROR_MESSAGE);
+      return false;
+    }
+
+    // check powerful
+    if (! $this->checkPasswordPower($password))
+      return false;
+
+    $this->model->password = $password;
+    return $password;
+  }
+
+    private function checkSamePassword($pass, $rePass){
+
+        validation::sanitizeString($rePass);
+
+        if ($pass !== $rePass){
+          $this->messenger->addMessage("Two passwords are not matching", Messenger::WARNING_MESSAGE);
+          return false;
+        }
+          return true;
+    }
+
+    private function validatePhoneNumber(){
+      $number = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
+
+      if (! is_integer((int)$number) || !validation::between(strlen($number),11,15))
+      {
+        $this->messenger->addMessage("Invalid Phone Number", Messenger::ERROR_MESSAGE);
+        return false;
+      }
+      $this->model->phoneNumber = $number;
+      return true;
+    }
+
+    private function validateData(){
+      $condition = true;
+
+      // validating Selected Group
+      $this->validateChosenGroup();
+
+      // validating username
+      $condition = $this->validateUsername();
+
+      // validating Email
+      $condition = $this->validateEmail();
+
+      // validating Password
+      $password  = $this->validatePassword();
+
+      // validating Confirm Password
+      $condition = $this->checkSamePassword($password, $_POST['CPassword']);
+
+      // validating Phone Number
+      if (! empty($_POST['phone']))
+        $condition = $this->validatePhoneNumber();
+
+      return $condition;
+    }
+
+    private function checkAddSuccess(){
+
+      $returnCode = $this->model->add();
+
+      switch ($returnCode)
+      {
+        case userModel::SUCCESS_ADD_CODE :
+          $this->messenger->addMessage("User has been added successfully.");
+        break;
+        case userModel::FAIL_ADD_CODE :
+          $this->messenger->addMessage("Failed to add this user.", Messenger::ERROR_MESSAGE);
+        break;
+        case userModel::EMAIL_EXIT_ERROR :
+          $this->messenger->addMessage("Email is already exist.", Messenger::WARNING_MESSAGE);
+        break;
+        case userModel::USERNAME_EXIT_ERROR :
+          $this->messenger->addMessage("Username is used.", Messenger::WARNING_MESSAGE);
+        break;
+        case userModel::PHONE_NUMBER_EXIT_ERROR :
+          $this->messenger->addMessage("Phone Number is used before.", Messenger::WARNING_MESSAGE);
+          break;
+      }
+
+    }
+
+    public function add(){
+
+    $this->pageTitle = "Add User";
+    $this->data['groups'] = $this->groupModel->fetchModelRecords();
+
+    if (isset($_POST['submit'])) {
+
+      if(! $this->validateData()){
+        $this->view();
+        return;
+      }
+      $this->checkAddSuccess();
+    }
+    $this->view();
+  }
+
+    // To avoid if anyone from playing in the URL
+    private function checkIdValidity(){
+      if (!isset($this->parameters[0]))
+        $this->redirectToHomePage();
+
+      $this->model->id = filter_var($this->parameters[0], FILTER_SANITIZE_NUMBER_INT);
+
+      if (empty($this->model->id))
+        $this->redirectToHomePage();
+    }
+
+    public function delete(){
+      $this->checkIdValidity();
+
+      if($this->model->deleteByPK())
+        $this->messenger->addMessage( "User is Removed successfully.");
+      else
+        $this->messenger->addMessage("Failed to remove this user.", Messenger::ERROR_MESSAGE);
+
+      helpers::reDirectAfterTime($this->controller, 2);
+      $this->main();
     }
 
     public function register(){
@@ -102,8 +328,6 @@ class user extends controller
 
         helpers::reDirect("../admin");
     }
-
-
 
     public function getErrors(){
         return $this->errors;
--- app/models/user.php
+++ app/models/user.php
@@ -6,26 +6,86 @@ use PDO;
 
 class user extends model
 {
+  public $id;
+  public $username;
+  public $password;
+  public $email;
+  public $phoneNumber	;
+  public $joinDate	;
+  public $groupId	;
+  public $status	;
 
-    public function checkUserExist($email, $password)
+  const SUCCESS_ADD_CODE = 0;
+  const EMAIL_EXIT_ERROR = 1;
+  const USERNAME_EXIT_ERROR = 2;
+  const PHONE_NUMBER_EXIT_ERROR = 3;
+  const FAIL_ADD_CODE = 4;
+
+
+
+  public function __construct($db)
+  {
+    parent::__construct($db);
+    $this->tableName = "user";
+    $this->primaryKey = 'id';
+    $this->tableSchema = array('username', 'password', 'email', 'phone_number', 'group_id',);
+    $this->phoneNumber = null;   // optional
+  }
+
+  public function edit(){
+    $data = array_combine($this->tableSchema, [
+        $this->username, $this->password, $this->email, $this->phoneNumber,$this->groupId
+    ]);
+    $condition = [$this->primaryKey => $this->id];
+    return $this->db->update($this->tableName, $data, $condition);
+  }
+
+  private function isEmailExist(){
+    $query = "SELECT `{$this->primaryKey}` FROM `{$this->tableName}` WHERE `email` = ? ";
+    $conditions = [$this->email];
+    $count = $this->db->count($query, $conditions);
+    return $count > 0;
+  }
+
+  private function isUsernameExist(){
+    $query = "SELECT `{$this->primaryKey}` FROM `{$this->tableName}` WHERE `username` = ? ";
+    $conditions = [$this->username];
+    $count = $this->db->count($query, $conditions);
+    return $count > 0;
+  }
+
+  private function isPhoneNumberExist(){
+    $query = "SELECT `{$this->primaryKey}` FROM `{$this->tableName}` WHERE `phone_number` = ? ";
+    $conditions = [$this->phoneNumber];
+    $count = $this->db->count($query, $conditions);
+    return $count > 0;
+  }
+
+  public function add()
     {
-        $query = "SELECT password from user where email = ?";
-        $result = $this->connect->run($query, [$email])->fetch(PDO::FETCH_ASSOC);
-        if (isset($result['password']))
-            return $result['password'] == $password;
-        else return false;
+      $nonExistingUser = true;
+      if ($this->isEmailExist())
+        return self::EMAIL_EXIT_ERROR;
+      if ($this->isUsernameExist())
+        return self::USERNAME_EXIT_ERROR;
+      if ( $this->phoneNumber != "NULL" && $this->isPhoneNumberExist())
+        return self::PHONE_NUMBER_EXIT_ERROR;
+
+      $data = array_combine($this->tableSchema, [
+          $this->username, $this->password, $this->email, $this->phoneNumber,$this->groupId
+      ]);
+      if ($this->db->insert($this->tableName, $data))
+        return self::SUCCESS_ADD_CODE;
+      return self::FAIL_ADD_CODE;
     }
 
-    // data is an associated array
-    public function addUser($data)
-    {
-        $this->connect->insert('user', $data);
-    }
+  public function addUserMessage(){
 
-    public function getUserName($email)
-    {
-        $query = "SELECT `name` from `user` where email = ?";
-        $result = $this->connect->run($query, [$email])->fetch(PDO::FETCH_ASSOC);
-        return $result['name'];
-    }
+  }
+
+  public function getUserGroups(){
+    $query = "SELECT `user`.`id`,`username`, `phone_number`,`email`, `last_login`,`join_date`,users_groups.name as groupName
+                from `user` INNER JOIN users_groups ON `user`.`group_id` = users_groups.id;";
+    return $this->db->run($query)->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
