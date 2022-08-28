ddc6b705ded254a23fd85f1ef7ba8b88c4dd0a07
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2022-08-28 14:27:50 +0200

    Some authentication edits

--- app/controllers/authentication.php
+++ app/controllers/authentication.php
@@ -21,42 +21,50 @@ class authentication extends controller
   {
     $this->login();
   }
+
+  private function isUserFound(){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    return $this->model->authenticate($username, $password, $userData);
+  }
+
+  private function handleResult($isUserFound){
+
+    switch ($isUserFound)
+    {
+      case UserModel::SUCCESS_LOGIN:
+        $this->messenger->addMessage("Success Login, you'll be redirected to your page.");
+        helpers::reDirect("home/main");
+        break;
+      case UserModel::DISABLED_USER:
+        $this->messenger->addMessage("Sorry, You are disabled.", Messenger::WARNING_MESSAGE);
+        break;
+      case UserModel::NOT_FOUND_USER:
+        $this->messenger->addMessage("Email or Password is wrong.", Messenger::ERROR_MESSAGE);
+        break;
+    }
+
+  }
+
   public function login(){
 
+    // if the user is already logged in, redirect him to the home page
     if(\MVC\core\Authentication::getInstance()->isAuthorized())
-      helpers::reDirect("userGroups");      // todo-me: check if is authorized-->go to his privilege direct
+      helpers::reDirect("home/main");
 
     $this->pageTitle = 'Login';
-
     if (isset($_POST['submit']))
     {
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-      $found = $this->model->authenticate($username, $password, $userData);
-
-      switch ($found)
-      {
-        case UserModel::SUCCESS_LOGIN:
-          $this->messenger->addMessage("Success Login, you'll be redirected to your page.");
-          helpers::reDirect("userGroups");  //todo-me: change it to his privilege url_link
-         break;
-        case UserModel::DISABLED_USER:
-          $this->messenger->addMessage("Sorry, You are disabled.", Messenger::WARNING_MESSAGE);
-          break;
-        case UserModel::NOT_FOUND_USER:
-          $this->messenger->addMessage("Email or Password is wrong.", Messenger::ERROR_MESSAGE);
-          break;
-      }
+      $isFound = $this->isUserFound();
+      $this->handleResult($isFound);
     }
     $this->view();
   }
+
   public function logout(){
-    if (\MVC\core\Authentication::getInstance()->isAuthorized())
-    {
       session::stop();
       helpers::reDirect("authentication/login");
-    }
-
   }
 
 
